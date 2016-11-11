67528982bea2ec96c21ef4bdb2a3a1f2b0cc65cc
Eresse <eresse@dooba.io>
2016-11-11 23:28:09 +0100

    fixed clean script

--- clean.sh
+++ clean.sh
@@ -4,10 +4,9 @@
 rm -rfv eagle/lbr/*.l#*
 rm -rfv eagle/proj/*/*.s#*
 rm -rfv eagle/proj/*/*.b#*
-rm -rfv eagle/proj/*/*.G{B,T}{L,O,S}
+rm -rfv eagle/proj/*/*.G{B,T}{L,O,S,P}
 rm -rfv eagle/proj/*/*.GML
 rm -rfv eagle/proj/*/*.GKO
-rm -rfv eagle/proj/*/*.GTP
 rm -rfv eagle/proj/*/*.TXT
 rm -rfv eagle/proj/*/*.dri
 rm -rfv eagle/proj/*/*.gpi
