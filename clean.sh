c0837b6c28930c781068fbaf46584a0ed52f3d96
Eresse <eresse@dooba.io>
2016-11-11 23:26:48 +0100

    Added Gerber files to clean script

--- clean.sh
+++ clean.sh
@@ -4,3 +4,10 @@
 rm -rfv eagle/lbr/*.l#*
 rm -rfv eagle/proj/*/*.s#*
 rm -rfv eagle/proj/*/*.b#*
+rm -rfv eagle/proj/*/*.G{B,T}{L,O,S}
+rm -rfv eagle/proj/*/*.GML
+rm -rfv eagle/proj/*/*.GKO
+rm -rfv eagle/proj/*/*.GTP
+rm -rfv eagle/proj/*/*.TXT
+rm -rfv eagle/proj/*/*.dri
+rm -rfv eagle/proj/*/*.gpi
